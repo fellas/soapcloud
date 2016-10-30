870278a606b5d271f9c191d7917bbe8717d6d04e
fellas <fellas88@gmail.com>
2016-10-30 18:26:45 -0500

    Update serverSOAP2.php

--- serverSOAP2.php
+++ serverSOAP2.php
@@ -3,7 +3,7 @@ include_once("clasesServicios.php");
 ini_set("soap.wsdl_cache_enabled", "0");
 $directorioWSDL="directorioWsdl/miWsdl2.wsdl";
 $parametros = array (
-	"uri"=>"http://ejemplo2-soapcc.44fs.preview.openshiftapps.com/",
+	//"uri"=>"http://ejemplo2-soapcc.44fs.preview.openshiftapps.com/",
 	"soap_version"=>SOAP_1_1
 );
 $ObjetoServer = new SoapServer($directorioWSDL,$parametros);
