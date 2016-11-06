8b347b4ce3a066a9790e3bb3f6d006382b5c5d55
fellas <fellas88@gmail.com>
2016-11-05 21:23:50 -0500

    Update serverSOAP3.php

--- serverSOAP3.php
+++ serverSOAP3.php
@@ -1,7 +1,7 @@
 <?php
 include_once("clasesServicios.php");
 ini_set("soap.wsdl_cache_enabled", "0");
-$directorioWSDL="directorioWsdl/miWsdl2.wsdl";
+$directorioWSDL="directorioWsdl/miWsdl3.wsdl";
 $parametros = array (
 	//"uri"=>"http://cloudsoap-davidenq.rhcloud.com/",
 	"soap_version"=>SOAP_1_1
