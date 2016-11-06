12100eec9ed786fe847fa500f6844c7a4a7b4cc6
fellas <fellas88@gmail.com>
2016-11-05 21:23:07 -0500

    serverSOAP3.php
    ejemplo para openshifth

---
+++ serverSOAP3.php
@@ -0,0 +1,12 @@
+<?php
+include_once("clasesServicios.php");
+ini_set("soap.wsdl_cache_enabled", "0");
+$directorioWSDL="directorioWsdl/miWsdl2.wsdl";
+$parametros = array (
+	//"uri"=>"http://cloudsoap-davidenq.rhcloud.com/",
+	"soap_version"=>SOAP_1_1
+);
+$ObjetoServer = new SoapServer($directorioWSDL,$parametros);
+$ObjetoServer->setClass("servicios");
+$ObjetoServer->handle();
+?>
